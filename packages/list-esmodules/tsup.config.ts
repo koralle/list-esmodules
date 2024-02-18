d7790a03d8e43d875e87ee58c22eff3166b5d761
koralle <mkhd7930@gmail.com>
2024-02-18 19:02:56 +0900

    feat: add `tsup.config.ts`

---
+++ packages/list-esmodules/tsup.config.ts
@@ -0,0 +1,22 @@
+import { match } from "ts-pattern";
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entryPoints: ["src/index.ts"],
+  dts: true,
+  format: ["esm", "cjs"],
+  sourcemap: true,
+  treeshake: true,
+  minify: true,
+  clean: true,
+  shims: true,
+  outExtension: ({ format }) => {
+    return {
+      js: match(format)
+        .with("cjs", () => ".cjs")
+        .with("esm", () => ".mjs")
+        .with("iife", () => ".iife")
+        .exhaustive(),
+    };
+  },
+});
