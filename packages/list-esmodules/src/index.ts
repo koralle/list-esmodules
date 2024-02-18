7dd2a949953cd135f8b5652d5a98f30d35597338
koralle <mkhd7930@gmail.com>
2024-02-19 01:25:39 +0900

    fix: index.ts

--- packages/list-esmodules/src/index.ts
+++ packages/list-esmodules/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import { execa } from "execa";
+
+export const lsEsModules = async () => {
+  const { stdout } = await execa("pnpm", ["ls", "-r", "--parseable"], {});
+
+  return stdout as string[];
+};
